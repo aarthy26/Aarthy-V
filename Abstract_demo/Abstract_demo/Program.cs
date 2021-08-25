815ad5a10d293c550b98c75e0ee35b128fbf13d7
aarthy26 <aarthy.v@gavstech.com>
2021-08-25 17:50:58 +0530

    Add files via upload

---
+++ Abstract_demo/Abstract_demo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_demo
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            contractor obj = new contractor();
+            obj.salary();
+            obj.annual();
+            Console.ReadLine();
+        }
+    }
+}
