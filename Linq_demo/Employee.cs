98eb2aea3890f7baca6f429933fa21e0240a4cc9
aarthy26 <aarthy.v@gavstech.com>
2021-08-25 17:35:00 +0530

    Add files via upload

---
+++ Linq_demo/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_demo
+{
+  public  class Employee
+    {
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public int id { get; set; }
+        public string dept { get; set; }
+        public string qualification { get; set; }
+        public int salary { get; set; }
+
+
+
+    }
+}
