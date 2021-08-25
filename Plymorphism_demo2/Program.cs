d523ffd8c8ba309d6a1cfc640a303c0e0923baca
aarthy26 <aarthy.v@gavstech.com>
2021-08-25 17:57:25 +0530

    Add files via upload

---
+++ Plymorphism_demo2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plymorphism_demo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account A1 = new Amount();
+            A1.balance();
+            
+            Vehicle V1 = new Car("TN004",1980,"petrol");
+            V1.start();
+            V1.Move();
+            V1.stop();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Plymorphism_demo2/Vehicle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plymorphism_demo2
+{
+   public class Vehicle
+    {
+        public string Regno;
+        public int Modelyear;
+        public string Fueltype;
+        public Vehicle(string Regno,int Modelyear,string Fueltype)
+        {
+            this.Regno = Regno;
+            this.Modelyear = Modelyear;
+            this.Fueltype = Fueltype;
+        }
+        public virtual void start()
+        {
+            Console.WriteLine("the vehicle is started");
+        }
+        public virtual void Move()
+        {
+            Console.WriteLine("the vechicle is moved");
+        }
+        public virtual void stop()
+        {
+            Console.WriteLine("the vechile is stopped");
+        }
+    }
+    public class Car: Vehicle
+    {
+        public Car(string Regno, int Modelyear, string Fueltype):base( Regno, Modelyear, Fueltype)
+        {
+            this.Regno = Regno;
+            this.Modelyear = Modelyear;
+            this.Fueltype = Fueltype;
+        }
+        public override void start()
+        {
+            Console.WriteLine("the Car is started");
+        }
+        public override void Move()
+        {
+            Console.WriteLine("the Car is moved");
+        }
+        public override void stop()
+        {
+            Console.WriteLine("the Car is stopped");
+        }
+    }
+        
+}
