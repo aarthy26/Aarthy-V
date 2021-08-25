d476628e72bfc47f47fac8eb7c758a60ea26d3ef
aarthy26 <aarthy.v@gavstech.com>
2021-08-25 17:35:39 +0530

    Add files via upload

---
+++ Interface_demo/Demo1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_demo    //multiple inheritance
+{
+   interface IDemo1
+    {
+        void mymethod();
+    }
+    interface IDemo2
+    {
+        void mymethod2();
+    }
+    class Democlass:IDemo1,IDemo2
+    {
+        public void mymethod()
+        {
+            Console.WriteLine("My firstmethod");
+        }
+        public void mymethod2()
+        {
+            Console.WriteLine("My secondmethod");
+        }
+    }
+}
