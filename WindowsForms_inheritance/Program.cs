79b52a42f135c9ddce77623587cf5ef9d4f4b403
aarthy26 <aarthy.v@gavstech.com>
2021-08-25 18:01:14 +0530

    Add files via upload

---
+++ WindowsForms_inheritance/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms_inheritance
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+        //public static void Main(string[] args)
+        //{
+        //    Car c1 = new Car();
+        //    c1.Name = "BMW";
+        //    c1.Color = "Red";
+        //    c1.Number = "TN23456";
+        //    c1.Year = 2000;
+        //    Console.WriteLine(c1.Name);
+        //    Console.WriteLine(c1.Color);
+        //    Console.WriteLine(c1.Number);
+        //    Console.WriteLine(c1.Year);
+        //    Console.ReadLine();
+        //}
+    }
+}
