9ee008c6e981734dd44eaaf9f0c3bbb50c9289d2
aarthy26 <aarthy.v@gavstech.com>
2021-08-25 17:23:34 +0530

    Add files via upload

---
+++ WindowsForms_inheritance/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms_inheritance
+{
+  public class Car:Form
+    {
+        private string name = "BMW";
+        private string color = "Black";
+        private string number = "TN23456";
+        private int year = 2000;
+
+        public string Name
+        {
+            get { return name; }
+            set { value = name; }
+        }
+        public string Color
+        {
+            get { return color; }
+            set { value = color; }
+        }
+        public string Number
+        {
+            get { return number; }
+            set { value = number; }
+        }
+        public int Year
+        {
+            get { return year; }
+            set { value = year; }
+        }
+    }
+}
---
+++ WindowsForms_inheritance/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms_inheritance
+{
+    public partial class Form1 : Car
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Cardetails();
+            Amountdetails();
+        }
+        public void Cardetails()
+        {
+            this.Name = Name;
+            this.Color = Color;
+            this.Number = Number;
+            this.Year = Year;
+            this.textBox1.Text = this.Name;
+            this.textBox2.Text = this.Color;
+            this.textBox3.Text = this.Number;
+            this.textBox4.Text = this.Year.ToString();
+        }
+        public void Amountdetails()
+        {
+            int iteratorWhile = 0;
+            int sum = 0;
+            int average = 0;
+
+            var expenses = new List<int> { 10000, 20000, 30000, 4000};
+            //for(int i=0;i<expenses.Count;i++)
+            //{
+            //    sum = sum + expenses[i];
+            //}
+            //while(iteratorWhile<expenses.Count())
+            //{
+            //    sum += expenses[iteratorWhile];
+            //    iteratorWhile += 1;
+            //}+zxzxc+
+            do
+            {
+                sum += expenses[iteratorWhile];
+                iteratorWhile += 1;
+            } while (iteratorWhile < expenses.Count());
+            average = sum / expenses.Count();
+            this.textBox5.Text = sum.ToString();
+            this.textBox6.Text = average.ToString();
+        }
+    }
+}
