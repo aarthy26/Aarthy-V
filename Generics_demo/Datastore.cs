f81ca69d5d0a64f6985ffe49e56621cf64edb8b7
aarthy26 <aarthy.v@gavstech.com>
2021-08-25 17:41:51 +0530

    Add files via upload

---
+++ Generics_demo/Datastore.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_demo
+{
+    //class Datastore<T>
+    //{
+    //    public T Data { get; set; }
+
+    //}
+    class Datastore<Tkey,Tstore>
+    {
+        public Tkey value { get; set; }
+        public Tstore Data { get; set; }
+    }
+
+}
---
+++ Generics_demo/Demo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_demo
+{
+    class Demo<T>
+    {
+        // define an array of generic type with length 6
+        T[] obj = new T[6];
+        int count = 0;
+
+        // adding items into generics type
+        public void Add(T items)
+        {
+            //checking length
+            if(count +1<7)
+            {
+                obj[count] = items;
+            }
+            count++;
+        }
+        //idexer for foreach statement iteration
+        public T this[int index]
+        {
+            get { return obj[index]; }
+            set { obj[index] = value; }
+        }
+    }
+}
