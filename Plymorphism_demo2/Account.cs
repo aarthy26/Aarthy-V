2494691634c2f444802550ce26dbdbea025b3e43
aarthy26 <aarthy.v@gavstech.com>
2021-08-25 17:29:33 +0530

    Add files via upload

---
+++ Plymorphism_demo2/Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plymorphism_demo2
+{
+   public class Account
+    {
+        public virtual void balance()
+        {
+            Console.WriteLine("The balance in my account is:" + 100);
+        }
+    }
+    public class Amount:Account
+    {
+        public override void balance()
+        {
+            Console.WriteLine("The balance in my account is:" + 500);
+        }
+    }
+}
