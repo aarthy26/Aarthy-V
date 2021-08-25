8119aad50f48fefdff894173a57d7e18c7ebd66c
aarthy26 <aarthy.v@gavstech.com>
2021-08-25 18:03:43 +0530

    Add files via upload

---
+++ EventsAndDelegates/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Video = new Video() { Title = "video1" };
+        }
+
+    
+    }
+}
