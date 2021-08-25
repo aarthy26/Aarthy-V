a1fdf76192cf925536b71d8bc5491e74c679e920
aarthy26 <aarthy.v@gavstech.com>
2021-08-25 17:10:52 +0530

    Add files via upload

---
+++ Abstract_demo/Abstract_demo/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_demo
+{
+    class contractor : Employee   //derived class(inherit from base class)
+    {
+        public override void salary()
+        {
+            Console.WriteLine("annual salary:" + 200000);
+        }
+    }
+
+    public abstract class Employee   //abstract class(base class)
+    {      
+        public abstract void salary();   //abstract method
+        public void annual()     //regular method
+        {
+            Console.WriteLine("annual salary:" + 400000);
+        }
+      
+    }
+}
