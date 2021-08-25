fdb0d68c628bdb39db434e7ef46b156cd90cb77a
aarthy26 <aarthy.v@gavstech.com>
2021-08-25 17:52:29 +0530

    Add files via upload

---
+++ Linq_demo/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employee = new List<Employee>()
+            {
+            new Employee { firstname = "Aishu", lastname = "Mary", id = 10326, dept = "application support", qualification = "B.tech",salary=20000 },
+            new Employee { firstname = "Jaya", lastname = "palani", id = 10327, dept = "accountance", qualification = "M.com",salary=10000 },
+            new Employee { firstname = "Priya", lastname = "Ramasamy", id = 10328, dept = "Quality", qualification = "B.Tech",salary=25000 },
+            new Employee { firstname = "Anitha", lastname = "Arumugam", id = 10329, dept = "networking", qualification = "B.E",salary=15000 },
+            new Employee { firstname = "Saranya", lastname = "prakash", id = 10330, dept = "security", qualification = "B.E",salary=28000 },
+            new Employee { firstname = "Divya", lastname = "krishnan", id = 10331, dept = "testing", qualification = "B.E",salary=30000 }
+        };
+            //basic query syntax
+            var records = from emp in employee
+                          orderby emp.firstname
+                          //orderby emp.qualification
+                          // group emp by emp.qualification
+                          //orderby emp.firstname descending
+                          //where emp.salary<20000
+                          select emp;
+           
+            //  foreach(var qualification in records)
+            //{
+            //    Console.WriteLine("qualification group", qualification.Key);
+            //    foreach (Employee emp in qualification)
+            //    {
+            //        Console.WriteLine("firstname", emp.firstname);
+            //    }
+            //}
+            foreach (var record in records)
+            {
+                Console.WriteLine(record.firstname + "-" + record.qualification);
+            }
+            Console.ReadLine();
+
+            //basic method syntax
+            //var records1 = employee.Select(s => s);
+            //Console.WriteLine("using the method syntax\n ***************");
+
+            //foreach (var record in records1)
+            //{
+            //    Console.WriteLine(record.firstname + "-" + record.qualification);
+            //}
+        }
+    }
+}
