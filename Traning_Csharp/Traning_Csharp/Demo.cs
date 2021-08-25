961e993ac9f656edd108a3b8ec8d67c6d035c87f
aarthy26 <aarthy.v@gavstech.com>
2021-08-25 17:27:12 +0530

    Add files via upload

---
+++ Traning_Csharp/Traning_Csharp/Demo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Traning_Csharp
+{
+  public class Demo : Form
+    {
+        public string Wheel()
+        {
+            return "yes";
+        }
+        public string ACaviability()
+        {
+            return "yes";
+        }
+        public string CallFacility()
+        {
+            return "No";
+        }
+
+    }
+}
---
+++ Traning_Csharp/Traning_Csharp/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Traning_Csharp
+{
+    public partial class Form1 : Demo
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Cardetails();
+        }
+        public void Cardetails()
+        {
+            this.textBox1.Text = "yes";
+            this.textBox2.Text = "yes";
+            this.textBox3.Text = "yes";
+        }
+    }
+}
