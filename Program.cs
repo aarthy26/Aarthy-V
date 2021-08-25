a6c7811c9d1de048b7ca5dc5d85ddf10002b95cd
aarthy26 <aarthy.v@gavstech.com>
2021-08-25 18:09:48 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_demo
+{
+  
+    class Program
+    {
+        enum DayOfWeek
+        {
+            Monday,      //0
+            Tuesday,     //1
+            Wednesday,    //2
+            Thursday,     //3
+            Friday        //4
+        }
+        static void Main(string[] args)
+        {
+            //DayOfWeek Myvar = new DayOfWeek.Monday;
+            int Myvar = (int)DayOfWeek.Monday;
+            Console.WriteLine(Myvar);          
+            Console.ReadLine();
+        }
+    }
+}
