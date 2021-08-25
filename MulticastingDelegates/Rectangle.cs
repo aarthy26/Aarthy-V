4a0737ea1e1abefd2fe1d7290af132c259cb4106
aarthy26 <aarthy.v@gavstech.com>
2021-08-25 17:54:00 +0530

    Add files via upload

---
+++ MulticastingDelegates/Rectangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MulticastingDelegates
+{
+    public delegate void RectDelegate(double width, double Height);    //defining a delegate
+    class Rectangle
+    {
+        public void GetArea(double Width, double Height)
+        {
+            Console.WriteLine("Area of the Rectangle:" + Width * Height);
+            
+        }
+        public void GetPerimeter(double width, double Height)
+        {
+            Console.WriteLine("Perimeter of the Rectangle:" + 2 * (width + Height));
+           
+        }
+    }
+}
