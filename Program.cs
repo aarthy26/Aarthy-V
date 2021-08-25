ecc7a139973822bc09d8e9809185d2e92456e459
aarthy26 <aarthy.v@gavstech.com>
2021-08-25 18:04:53 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,15 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EventsAndDelegates
+namespace MulticastingDelegatesDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var Video = new Video() { Title = "video1" };
+            Rectangle rect = new Rectangle();
+            rect.GetArea(2.4, 3.6);
+            rect.GetPerimeter(0.3, 1.2);
+            Console.ReadLine();
         }
-
-    
     }
 }
